83957d4b8f77e988f6c2889a6a754f73d5e6486d
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-07-05 12:13:09 +0100

    Changed 5 times

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,11 @@
-def fizz_buzz(num)
-  str = ''
-  str += 'Fizz' if (num % 5).zero?
-  str += 'Buzz' if (num % 3).zero?
-  str.empty? ? num : str
+def is_divisible_by_three?(number)
+  number % 3 == 0
+end
+
+def is_divisible_by_five?(number)
+  number % 5 == 0
+end
+
+def is_divisible_by_fifteen?(number)
+  number % 15 == 0
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,19 +1,26 @@
 require "fizzbuzz"
 
-describe "fizzbuzz" do
-  it "mutliple of 5 should equal Fizz" do
-    expect(fizz_buzz(5)).to eq("Fizz")
+describe 'FizzBuzz' do
+  context 'knows that a number is divisible by' do
+    it '3' do
+      expect(is_divisible_by_three?(3)).to be_truthy
+    end
+    it '5' do
+      expect(is_divisible_by_five?(5)).to be_truthy
+    end
+    it '15' do
+      expect(is_divisible_by_fifteen?(15)).to be_truthy
+    end
   end
-
-  it "multiple of 3 should equal Buzz" do
-    expect(fizz_buzz(3)).to eq("Buzz")
-  end
-
-  it "multiple of 3 and 5 should equal FizzBuzz" do
-    expect(fizz_buzz(15)).to eq("FizzBuzz")
-  end
-
-  it "should return back the same number" do
-    expect(fizz_buzz(89)).to eq(89)
+  context 'knows that number is not divisible by' do
+    it '3' do
+      expect(is_divisible_by_three?(1)).not_to be true
+    end
+    it '5' do
+      expect(is_divisible_by_five?(6)).not_to be true
+    end
+    it '15' do
+      expect(is_divisible_by_fifteen?(14)).not_to be true
+    end
   end
 end
